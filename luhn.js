348c80e2c1cde63d47ad697370eba0d2ab6ff042
jbellue <julien.bellue@payroc.com>
2023-03-22 10:16:10 +0100

    add luhn checker page

---
+++ luhn.js
@@ -0,0 +1,33 @@
+// define the elements to use them later 
+let cardInput, luhnResult;
+
+const luhnCheck = num => {
+    const arr = (num + '')
+        .split('')
+        .reverse()
+        .map(x => parseInt(x));
+    const lastDigit = arr.splice(0, 1)[0];
+    let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+    sum += lastDigit;
+    return sum % 10 === 0;
+};
+
+const runCheck = () => {
+    luhnResult.textContent = luhnCheck(cardInput.value) ?
+        "✔️ luhn passes" :
+        "❌ luhn fails";
+}
+
+const loadPageData = () => {
+    cardInput = document.getElementById("cardNumber");
+    luhnResult = document.getElementById("luhnResult");
+
+    cardInput.addEventListener("input", runCheck);
+
+    runCheck();
+};
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    loadPageData();
+});
